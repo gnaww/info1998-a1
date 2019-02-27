cfb2a42e9fdde9a63be0e52c61a7b4fefbfbb6ca
William Wang <wow7@cornell.edu>
2019-02-27 03:02:34 -0500

    refactored code and added sliding with navbar links

--- js/index.js
+++ js/index.js
@@ -2,42 +2,116 @@ let questionIndex = 0;
 let questionMaxIndex = 3;
 let questionIdArray = ['name', 'major', 'year', 'forward'];
 
-function slideLeft() {
-    console.log(questionIdArray[questionIndex])
-    let currentQuestion = document.getElementById(questionIdArray[questionIndex]);
-    currentQuestion.className += " hidden";
-    currentQuestion.classList.remove("slideLeft");
-    currentQuestion.classList.remove("slideRight");
-    currentQuestion.classList.remove("shown");
-
-    questionIndex--;
-    if (questionIndex < 0) {
-        questionIndex = questionMaxIndex;
+function goTo(question) {
+    if (question === 'name') {
+        if (questionIndex !== 0) {
+            hideCurrentQuestion();
+            let goToQuestion = document.getElementById('name');
+            goToQuestion.classList.remove("hidden");
+            goToQuestion.className += " shown slideLeft";
+            questionIndex = 0;
+        }
     }
+    else if (question === 'major') {
+        if (questionIndex !== 1) {
+            hideCurrentQuestion();
 
-    console.log(questionIdArray[questionIndex])
-    let nextQuestion = document.getElementById(questionIdArray[questionIndex]);
-    nextQuestion.classList.remove("hidden");
-    nextQuestion.className += " shown slideLeft";
+            let goToQuestion = document.getElementById('major');
+            goToQuestion.classList.remove("hidden");
+            
+            if (questionIndex < 1) {
+                goToQuestion.className += " shown slideRight";
+            }
+            else {
+                goToQuestion.className += " shown slideLeft";
+            }
+
+            questionIndex = 1;
+        }
+    }
+    else if (question === 'year') {
+        if (questionIndex !== 2) {
+            hideCurrentQuestion();
+            let goToQuestion = document.getElementById('year');
+            goToQuestion.classList.remove("hidden");
+            
+            if (questionIndex < 2) {
+                goToQuestion.className += " shown slideRight";
+            }
+            else {
+                goToQuestion.className += " shown slideLeft";
+            }
+
+            questionIndex = 2;
+        }
+    }
+    else if (question === 'forward') {
+        if (questionIndex !== 3) {
+            hideCurrentQuestion();
+            let goToQuestion = document.getElementById('forward');
+            goToQuestion.classList.remove("hidden");
+            
+            if (questionIndex < 3) {
+                goToQuestion.className += " shown slideRight";
+            }
+            else {
+                goToQuestion.className += " shown slideLeft";
+            }
+
+            questionIndex = 3;
+        }
+    }
+    else if (question === 'ice') {
+        if (questionIndex !== 4) {
+            hideCurrentQuestion();
+            let goToQuestion = document.getElementById('ice');
+            goToQuestion.classList.remove("hidden");
+
+            if (questionIndex < 4) {
+                goToQuestion.className += " shown slideRight";
+            }
+            else {
+                goToQuestion.className += " shown slideLeft";
+            }
+
+            questionIndex = 4;
+        }
+    }
 }
 
-function slideRight() {
-    console.log(questionIdArray[questionIndex])
-    let currentQuestion = document.getElementById(questionIdArray[questionIndex]);
-    currentQuestion.className += " hidden";
-    currentQuestion.classList.remove("slideLeft");
-    currentQuestion.classList.remove("slideRight");
-    currentQuestion.classList.remove("shown");
+function slide(direction) {
+    hideCurrentQuestion();
 
-    questionIndex++;
-    if (questionIndex > questionMaxIndex) {
-        questionIndex = 0;
+    if (direction === 'left') {
+        questionIndex--;
+        if (questionIndex < 0) {
+            questionIndex = questionMaxIndex;
+        }
+    }
+    else if (direction === 'right') {
+        questionIndex++;
+        if (questionIndex > questionMaxIndex) {
+            questionIndex = 0;
+        }
     }
 
-    console.log(questionIdArray[questionIndex])
     let nextQuestion = document.getElementById(questionIdArray[questionIndex]);
     nextQuestion.classList.remove("hidden");
-    nextQuestion.className += " shown slideRight";
+
+    if (direction === 'left') {
+        nextQuestion.className += " shown slideLeft";
+    }
+    else if (direction === 'right') {
+        nextQuestion.className += " shown slideRight";
+    }
+}
+
+function hideCurrentQuestion() {
+    let question = document.getElementById(questionIdArray[questionIndex]);
+    question.className += " hidden";
+    question.classList.remove("slideLeft");
+    question.classList.remove("slideRight");
+    question.classList.remove("shown");
 }
 
 function main() {
@@ -54,14 +128,13 @@ function showIceCream(seconds) {
 }
 
 function addIceCream() {
-    console.log('hi')
     document.getElementById('container').innerHTML += 
     `<div id="ice" class="question hidden">
         <h1>What's my favorite ice cream?</h1>
         <p>My favorite ice cream flavor is Cookies N' Cream.</p>
      </div>`;
     document.getElementById('navigation').innerHTML +=
-    "<li class='shadow'><a href='#ice' id='iceNav'>Ice Cream</a></li>";
+    "<li class='shadow'><a onclick=\"goTo('ice')\">Ice Cream</a></li>";
     questionMaxIndex = 4;
     questionIdArray.push('ice');
 }
